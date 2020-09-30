640917f19bf8b2934a186bddfc269e7c549fc446
Emiko Johansson <emjo1804@student.miun.se>
2020-09-30 15:50:07 +0200

    LetterKeyViewModel

--- Excersise ItemsControl/ViewModels/LetterKeyViewModel.cs
+++ Excersise ItemsControl/ViewModels/LetterKeyViewModel.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Excersise_ItemsControl.ViewModels.Base;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Excersise_ItemsControl.ViewModels
 {
-    class LetterKeyViewModel
+    class LetterKeyViewModel : BaseViewModel
     {
+        public char Content { get; set; }
+        public ICommand LetterKeyCommand { get; set; }
+        public object CommandParameter => Content;
+
+        public double Opacity { get; set; } = 1;
+        public string Foreground { get; set; } = "White";
+        public string FontWeight { get; set; } = "Normal";
+        public bool IsEnabled { get; set; } = true;
+
+        public readonly int QwertyOrder;
+
+        public LetterKeyViewModel(int qwertyOrder, char content, ICommand keyCommand)
+        {
+            QwertyOrder = qwertyOrder;
+            Content = content;
+            LetterKeyCommand = keyCommand;
+        }
+
     }
 }

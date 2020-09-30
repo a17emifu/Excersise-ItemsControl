b7eea3c67b07663e079fb3d19d2ee29af93ed11a
Emiko Johansson <emjo1804@student.miun.se>
2020-09-30 15:59:26 +0200

    LetterKeyboardViewModel

--- Excersise ItemsControl/ViewModels/LetterKeyboardViewModel.cs
+++ Excersise ItemsControl/ViewModels/LetterKeyboardViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Excersise_ItemsControl.ViewModels.Base;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace Excersise_ItemsControl.ViewModels
 {
-    class LetterKeyboardViewModel
+    class LetterKeyboardViewModel: BaseViewModel
     {
+        private static readonly char[] _lettersQWERTY = "QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM".ToCharArray();
+        private readonly ICommand _keyCommand;
+
+        public ObservableCollection<LetterKeyViewModel> Keys { get; set; }
+
+        public LetterKeyboardViewModel(ICommand keyCommand)
+        {
+            _keyCommand = keyCommand;
+
+            Keys = new ObservableCollection<LetterKeyViewModel>();
+
+            int qwertyIndex = 0;
+            foreach (var c in _lettersQWERTY)
+            {
+                Keys.Add(new LetterKeyViewModel(qwertyIndex, c, _keyCommand));
+                qwertyIndex++;
+            }
+        }
+
+        public void MarkLetterUsed(char letter)
+        {
+            var key = Keys.SingleOrDefault(o => o.Content == letter);
+
+            key.Opacity = 0.3;
+            //key.Foreground = isCorrect ? "Green" : "Red";
+            key.FontWeight = "Bold";
+            key.IsEnabled = false;
+        }
     }
 }

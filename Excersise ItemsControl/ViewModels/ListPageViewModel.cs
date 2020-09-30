db7e8e6b89ce356b0328d129879a94259c0ab267
Emiko Johansson <emjo1804@student.miun.se>
2020-09-30 19:30:22 +0200

    ListPageVM + Koppling

---
+++ Excersise ItemsControl/ViewModels/ListPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Excersise_ItemsControl.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excersise_ItemsControl.ViewModels
+{
+    class ListPageViewModel : BaseViewModel
+    {
+    }
+}

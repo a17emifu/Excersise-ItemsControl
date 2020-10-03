1215a0ac6f9977e879d0e8a438036643f775faf3
Emiko Johansson <emjo1804@student.miun.se>
2020-10-03 09:05:48 +0200

    MasterMind: View

---
+++ Excersise ItemsControl/Views/MasterMind/PegUC.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Excersise_ItemsControl.ViewModels.MasterMind;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Excersise_ItemsControl.Views.MasterMind
+{
+    /// <summary>
+    /// PegUC.xaml の相互作用ロジック
+    /// </summary>
+    public partial class PegUC : UserControl
+    {
+        public PegUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
